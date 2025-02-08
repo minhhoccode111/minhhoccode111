9d27d15e1ddb1fac86df29f5b6d739b15813225e
minhhoccode111 <minhhoccode111@gmail.com>
2025-02-09 00:03:03 +0700

    chore

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from 'vite';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  base: './minhhoccode111', // because we deploy on gh-pages
+  base: '/minhhoccode111/', // because we deploy on gh-pages
   plugins: [react(), viteTsconfigPaths()],
   server: {
     port: 3000,
